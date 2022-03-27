b63f771846efb85f80765a89d47a0c0d81e92804
Māris Muižnieks <maris.muiznieks@lumi.moe>
2022-03-27 21:08:27 +0300

    Cleanup cpu.c

--- src/cpu.c
+++ src/cpu.c
@@ -18,46 +18,6 @@ static void cpu_fetch_inst()
     ctx.cur_inst = instruction_by_opcode(ctx.cur_opcode);
 }
 
-/*
-static void cpu_fetch_data()
-{
-    ctx.mem_dest = 0;
-    ctx.dest_is_mem = false;
-
-    if (ctx.cur_inst == NULL) {
-        return;
-    }
-
-    switch(ctx.cur_inst->mode) {
-        case AM_IMP:
-            return;
-        case AM_R:
-            ctx.fetched_data  = cpu_read_reg(ctx.cur_inst->reg_1);
-            return;
-        case AM_R_D8:
-            ctx.fetched_data = bus_read(ctx.reg.pc);
-            emu_cycles(1);
-            ctx.reg.pc++;
-            return;
-        case AM_D16: {
-            u16 lo = bus_read(ctx.reg.pc);
-            emu_cycles(1);
-
-            u16 hi = bus_read(ctx.reg.pc + 1);
-            emu_cycles(1);
-
-            ctx.fetched_data = lo | (hi << 8);
-            ctx.reg.pc+=2;
-            return;
-        }
-        default:
-            printf("Err... Nezinama adressesanas Metode! %d (%02X)\n", ctx.cur_inst->mode, ctx.cur_opcode);
-            exit(-7);
-            return;
-    }
-}
-*/
-
 static void cpu_execute()
 {
     IN_PROC proc = inst_get_processor(ctx.cur_inst->type);
