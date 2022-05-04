e48f71331900c6a72b8355864ee93dc0206c4139
Maris Muiznieks <maris.muiznieks@lumi.moe>
2022-05-04 17:59:25 +0300

    Added stack.c

--- src/cpu_proc.c
+++ src/cpu_proc.c
@@ -40,6 +40,17 @@ static void proc_ld(cpu_context *ctx)
     cpu_set_reg(ctx->cur_inst->reg_1, ctx->fetched_data);
 }
 
+static void proc_ldh(cpu_context *ctx)
+{
+    if (ctx->cur_inst->reg_1 == RT_A) {
+        cpu_set_reg(ctx->cur_inst->reg_1, bus_read(0xFF00 | ctx->fetched_data));
+    } else {
+        bus_write(ctx->mem_dest, ctx->reg.a);
+    }
+    emu_cycle(1);
+}
+
+
 static void proc_xor(cpu_context *ctx)
 {
     ctx->reg.a ^= ctx->fetched_data & 0xFF;
@@ -66,6 +77,19 @@ static bool proc_check_cond(cpu_context *ctx)
     return false;
 }
 
+// move to cpu_util.h
+static void goto_addr(cpu_context *ctx, u16 addr, bool pushpc)
+{
+    if (proc_check_cond(ctx)) {
+        if (pushpc) {
+            emu_cycles(2);
+            stack_push16(ctx->reg.pc);
+        }
+        ctx->reg.pc = addr;
+        emu_cycles(1);
+    }
+}
+
 static void proc_jp(cpu_context *ctx)
 {
     if (proc_check_cond(ctx)) {
@@ -74,13 +98,87 @@ static void proc_jp(cpu_context *ctx)
     }
 }
 
+static void proc_jr(cpu_context *ctx)
+{
+    char rel = (char)(ctx->fetched_data & 0xFF);
+    u16 addr = ctx->reg.pc + rel;
+    goto_addr(ctx, addr, false);
+}
+
+static void proc_call(cpu_context *ctx)
+{
+    goto_addr(ctx, ctx->fetched_data, true);
+}
+
+static void proc_rst(cpu_context *ctx)
+{
+    goto_addr(ctx, ctx->cur_inst->param, true);
+}
+
+static void proc_ret(cpu_context *ctx)
+{
+    if (ctx->cur_inst->cond != CT_NONE)
+        emu_cycles(1);
+    if (proc_check_cond(ctx)) {
+        u16 lo = stack_pop();
+        emu_cycle(1);
+        u16 hi = stack_pop();
+        emu_cycle(1);
+
+        u16 n = (hi << 8) | lo;
+        ctx->reg.pc = n;
+
+        emu_cycles(1);
+    }
+}
+
+static void proc_reti(cpu_context *ctx)
+{
+    ctx->int_master_enabled = true;
+    proc_ret(ctx);
+}
+
+static void proc_pop(cpu_context *ctx)
+{
+    u16 lo = stack_pop();
+    emu_cycles(1);
+    u16 hi = stack_pop();
+    emu_cycles(1);
+
+    u16 n = (hi << 8) | lo;
+    cpu_set_reg(ctx->cur_inst->reg_1, n);
+    if (ctx->cur_inst->reg_1 == RT_AF)
+        cpu_set_reg(ctx->cur_inst->reg_1, n & 0xFFF0);
+}
+
+static void proc_push(cpu_context *ctx)
+{
+    u16 hi = (cpu_read_reg(ctx->cur_inst->reg_1) >> 8) & 0xFF;
+    emu_cycles(1);
+    stack_push(hi);
+
+    u16 lo = cpu_read_reg(ctx->cur_inst->reg_1) & 0xFF;
+    emu_cycles(1);
+    stack_push(lo);
+
+    emu_cycles(1);
+}
+
 static IN_PROC processors[] = {
 [IN_NONE] = proc_none,
 [IN_NOP] = proc_nop,
 [IN_LD] = proc_ld,
+[IN_LDH] = proc_ldh,
 [IN_JP] = proc_jp,
 [IN_DI] = proc_di,
+[IN_POP] = proc_pop,
+[IN_PUSH] = proc_push,
+[IN_JR] = proc_jr,
+[IN_CALL] = proc_call,
+[IN_RET] = proc_ret,
+[IN_RETI] = proc_reti,
 [IN_XOR] = proc_xor
+
 };
 
 IN_PROC inst_get_processor(in_type type)
--- src/instr.c
+++ src/instr.c
@@ -1,6 +1,8 @@
 #include "instr.h"
 #include "cpu.h"
 
+// TODO add more instructions
+
 instruction instr[0x100] = {
     [0x00] = {IN_NOP, AM_IMP},
     [0x01] = {IN_LD, AM_R_D16, RT_BC},
